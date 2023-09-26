936bfff45a8270e82bebaf34786b0bbd882c5388
Tinica Adrian <118120924+adriantinica@users.noreply.github.com>
2023-09-26 22:02:48 +0300

    Update Main.java

--- e_shop_hibernate/src/main/java/student/example/Main.java
+++ e_shop_hibernate/src/main/java/student/example/Main.java
@@ -12,6 +12,9 @@ import org.hibernate.SessionFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import student.example.domain.Category;
+import student.example.domain.Currency;
+import student.example.domain.Money;
 import student.example.domain.Product;
 
 public class Main {
@@ -20,39 +23,37 @@ public class Main {
 
     EntityManagerFactory emf = Persistence.createEntityManagerFactory("e-shop-main");
     EntityManager em = emf.createEntityManager();
-    //Session session = em.
-
-    Product product1  = new Product();
-    product1.setName("First Product 1");
-
-    
-    
+    Category smartphoneCat = new Category("smartphones");
+    Product product = new Product("Samsung XXII", "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages",new Money(1000, "EUR"));
+    Currency eurCurr = new Currency("euro", "EUR", 978, 19.3622);
     
     //CRUD
 
     //CREATE
+
     em.getTransaction().begin();
-    em.persist(product1);
+
+    em.persist(product);
+    em.persist(smartphoneCat);
+    em.persist(eurCurr);
+
     em.getTransaction().commit();
 
 
-    //READ 
 
-    Product product  = em.find(Product.class, UUID.fromString("0b15e5b7-ddd0-4501-9192-217a5c164e7d"));
+
+
+
     
-    System.out.println(product.toString());
+    
+    
+    
+    
+    //READ 
+    //Product product  = em.find(Product.class, UUID.fromString("0b15e5b7-ddd0-4501-9192-217a5c164e7d"));
+    //System.out.println(product.toString());
+ 
 
-    // UPDATE
-     
-    em.getTransaction().begin();
-    product1.setName("Product 999");
-    em.getTransaction().commit();
-
-    //DELETE
-    //em.getTransaction().begin();
-    //Product product2  = em.find(Product.class, UUID.fromString("eb980ff6-329e-4b8a-a07d-6cc7b7e335a0"));
-    //em.remove(product2);
-    //em.getTransaction().commit();
 
 
     }
