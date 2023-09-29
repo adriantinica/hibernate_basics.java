f7dfcdcb0a7eff9f7206ed3bf4ce1a3d7deae242
Tinica Adrian <118120924+adriantinica@users.noreply.github.com>
2023-09-29 19:48:25 +0300

    Update Main.java

--- e_shop_hibernate/src/main/java/student/example/Main.java
+++ e_shop_hibernate/src/main/java/student/example/Main.java
@@ -1,10 +1,18 @@
-packagepackage student.example;
+ package student.example;
 
 import java.util.UUID;
+
+import org.hibernate.annotations.processing.Find;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import student.example.domain.Adress;
+import student.example.domain.Cart;
+import student.example.domain.CartItem;
+import student.example.domain.CartState;
 import student.example.domain.Category;
+import student.example.domain.Client;
 import student.example.domain.Currency;
 import student.example.domain.Money;
 import student.example.domain.Product;
@@ -18,37 +26,46 @@ public class Main {
 
 
     em.getTransaction().begin();
-        
-    Category smartphoneCat = new Category("smartphones");
-    em.persist(smartphoneCat);
-    Product product = new Product("Samsung XX", "image.jpg");
+   
+    Product product = new Product("Iphone 15", "image15.jpg");
     em.persist(product);
-    Money cost = new Money(1000, "USD");
+    Money cost = new Money(3200, "USD");
     em.persist(cost);
     product.setPrice(cost);
     Currency usdCurr = new Currency("Dolar S.U.A.", "USD", 840, 18.1679);
     em.persist(usdCurr);
-    product.setCategory(smartphoneCat);
+    
+    //Client client1 = new Client("Marry Jane");
+    //client1.getContactPhone().add("123456239");
+    //client1.getContactPhone().add("987654565");
+    //client1.setAdress(new Adress("New York", "Broadway", "86th"));
+    
+    Client client  = em.find(Client.class, UUID.fromString("9a18b982-dcbf-4da9-b13c-c915dfd2c5aa"));
+    Cart cart = new Cart();
+    em.persist(cart);
+    CartItem cartItem = new CartItem(product,(short) 2);
+    em.persist(cartItem);
+    cart.getCartitems().add(cartItem);
+    em.persist(cart);
+    client.getCarts().add(cart);
+    em.persist(client);
     
     //READ 
     //Product product  = em.find(Product.class, UUID.fromString("0b15e5b7-ddd0-4501-9192-217a5c164e7d"));
     //System.out.println(product.toString());
-
+    
     //DELETE
-    Product product1  = em.find(Product.class, UUID.fromString("41309d41-f61c-40ed-a11c-3a9c5051371b"));
-    em.remove(product1);
-    Product product2  = em.find(Product.class, UUID.fromString("bd218dee-5160-4732-bd18-7c10f0e9f42e"));
-    em.remove(product2);
-    Product product3  = em.find(Product.class, UUID.fromString("f2ced312-9423-43ba-8c54-2a18cbabf4a6"));
-    em.remove(product3);
-    Product product4  = em.find(Product.class, UUID.fromString("8bad1413-f088-4bdc-8eb5-347fb31114e0"));
-    em.remove(product4);
-    Product product5  = em.find(Product.class, UUID.fromString("16121f01-a96c-4500-8b8a-7a8ac39df275"));
-    em.remove(product5);
 
+    cart.setState(CartState.COMPLETED);
+
+    
     em.getTransaction().commit();
+    cart.getState();
+    System.out.println("Cart state: "+ cart.getState());
+    
 
 
 
     }
 }
+    
